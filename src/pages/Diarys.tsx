1bafc4e6f60077bc527d5484b480f087acc5f598
김명식 <kimmyeongsik@gimmyeongsig-ui-MacBookPro.local>
2022-12-26 19:20:13 +0900

    comment(Diarys.tsv): 주석 제거

--- src/pages/Diarys.tsx
+++ src/pages/Diarys.tsx
@@ -1,6 +1,5 @@
 import DiaryCard from '@/components/cards/DiaryCard';
 import { Diary } from '@/types/Interface';
-import axios, { AxiosResponse } from 'axios';
 import React, { useEffect, useState } from 'react';
 import { diaryStore } from '@/store/DiaryStore';
 
@@ -10,10 +9,6 @@ function Diarys() {
   const { storeDiarys } = diaryStore();
 
   const getDiary = () => {
-    // axios.get('http://localhost:3001/posts').then((res: AxiosResponse<Diary[]>) => {
-    //   console.log('받아온 리스트 정보 => ', res);
-    //   setDiarys(res.data);
-    // });
     setDiarys(storeDiarys);
   };
   // useEffect 의 뒤에 빈배열 []을 넣어놓으면 한번만 실행된다.
