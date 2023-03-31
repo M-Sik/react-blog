d252f2f3608fc5738d2ec5e98a85da17e1a5faed
김명식 <rlaaudtlr233@naver.com>
2023-03-31 19:55:13 +0900

    [refactor] router/index.tsx lazy함수 구조분해할당으로 가져옴

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,9 @@
 import { useRoutes } from 'react-router-dom';
-import React from 'react';
-const Home = React.lazy(() => import('@/pages/Home'));
-const AddDiary = React.lazy(() => import('@/pages/AddDiary'));
-const MainLayout = React.lazy(() => import('@/layouts/MainLayout'));
-const Diarys = React.lazy(() => import('@/pages/Diarys'));
+import { lazy } from 'react';
+const Home = lazy(() => import('@/pages/Home'));
+const AddDiary = lazy(() => import('@/pages/AddDiary'));
+const MainLayout = lazy(() => import('@/layouts/MainLayout'));
+const Diarys = lazy(() => import('@/pages/Diarys'));
 
 function Router() {
   const routes = useRoutes([
